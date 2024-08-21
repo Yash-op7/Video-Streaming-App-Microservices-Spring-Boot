20047e66106874297a5b77f2494046bddbbb743c
yash-op7 <yashm3375@gmail.com>
2024-08-22 04:57:02 +0530

    finished configuration of api gateway

--- api-gateway-service/src/main/java/com/yash_95/video_streaming_app/api_gateway_service/ApiGatewayServiceApplication.java
+++ api-gateway-service/src/main/java/com/yash_95/video_streaming_app/api_gateway_service/ApiGatewayServiceApplication.java
@@ -2,9 +2,10 @@ package com.yash_95.video_streaming_app.api_gateway_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryCli
+@EnableDiscoveryClient
 public class ApiGatewayServiceApplication {
 
 	public static void main(String[] args) {
