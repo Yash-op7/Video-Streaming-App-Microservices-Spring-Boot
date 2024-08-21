572247745f04e33d4367eba86de28143d626cef8
yash-op7 <yashm3375@gmail.com>
2024-08-22 03:20:26 +0530

    testing streaming service

---
+++ movie-streaming-service/src/main/java/com/yash_95/video_streaming_app/movie_streaming_service/controller/MovieStreamController.java
@@ -0,0 +1,35 @@
+package com.yash_95.video_streaming_app.movie_streaming_service.controller;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class MovieStreamController {
+
+    public static final String VIDEO_DIRECTORY = "/Users/yashmeena/Desktop/stuff/git repos/Video-Streaming-App-Microservices-Spring-Boot/assets/";
+
+    @GetMapping("/stream/{videoPath}")
+    public ResponseEntity<InputStreamResource> streamVideo(@PathVariable String videoPath) throws FileNotFoundException  {
+        System.out.println("video path is" + VIDEO_DIRECTORY + videoPath);
+
+        File file = new File(VIDEO_DIRECTORY+videoPath);
+        if(file.exists()) {
+            InputStreamResource inputStreamResource = new InputStreamResource(new FileInputStream(file));
+            return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType("video/mp4"))
+                .body(inputStreamResource);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
+}
