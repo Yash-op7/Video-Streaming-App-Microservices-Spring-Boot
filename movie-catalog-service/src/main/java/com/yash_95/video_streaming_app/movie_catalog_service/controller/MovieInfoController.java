b2363bfdbf226703b8696d8e2ed18e11f50e5f7d
yash-op7 <yashm3375@gmail.com>
2024-08-21 23:59:34 +0530

    movie-catalog-service: completed entity controller and model

---
+++ movie-catalog-service/src/main/java/com/yash_95/video_streaming_app/movie_catalog_service/controller/MovieInfoController.java
@@ -0,0 +1,31 @@
+package com.yash_95.video_streaming_app.movie_catalog_service.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yash_95.video_streaming_app.movie_catalog_service.model.MovieInfo;
+import com.yash_95.video_streaming_app.movie_catalog_service.model.MovieInfoRepository;
+
+
+@RestController
+public class MovieInfoController {
+    
+    @Autowired
+    private MovieInfoRepository repository;
+
+    @PostMapping("/movie-info/save")
+    public List<MovieInfo> saveAll(@RequestBody List<MovieInfo> movieInfoList) {
+        return repository.saveAll(movieInfoList);
+    }
+
+    @GetMapping("/movie-info/list")
+    public List<MovieInfo> getAll() {
+        return repository.findAll();
+    }    
+    
+}
