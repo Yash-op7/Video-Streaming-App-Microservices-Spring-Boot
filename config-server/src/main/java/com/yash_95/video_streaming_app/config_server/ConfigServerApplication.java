a02ca1b633a72a4e0f13cb38d32335e8e6d5ba67
yash-op7 <yashm3375@gmail.com>
2024-08-22 07:49:39 +0530

    initialized configserver

---
+++ config-server/src/main/java/com/yash_95/video_streaming_app/config_server/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.yash_95.video_streaming_app.config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
---
+++ config-server/src/test/java/com/yash_95/video_streaming_app/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yash_95.video_streaming_app.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
